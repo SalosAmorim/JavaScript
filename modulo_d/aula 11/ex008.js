33721d7af7d16debc0c4223a9b8b551ea7e299d2
SalosAmorim <salos.amorim@gmail.com>
2026-03-17 10:57:15 -0300

    create aula 11

---
+++ modulo_d/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var speed = 60
+console.log(`The car speed is ${speed}km/h`)
+if (speed > 60){ // condicao simples
+    console.log(`You got a speeding ticket!`)
+}
+console.log(`Always use seatbelt`)
---
+++ modulo_d/aula 11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Brazil'
+console.log(`Living in ${country}`)
+if (country == 'Brazil'){
+    console.log(`You are brazilian`)
+} else {
+    console.log(`You're not from here`)
+}
