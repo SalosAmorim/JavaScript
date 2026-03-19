7d47d5318c5859ae83691cf8bf1d069e311a4e76
SalosAmorim <salos.amorim@gmail.com>
2026-03-19 20:55:59 -0300

    Create aula 13 14

---
+++ modulo_e/aula 13-14/repeticao.js
@@ -0,0 +1,16 @@
+// i=1
+// while(i <= 5){
+//     console.log(`Number ${i}`)          //While
+//     i++
+// }
+
+// i=3
+
+// do {
+//     console.log(`${i}°`)                //Do While
+//     i--
+// } while(i>=1)
+
+// for(var i = 1; i <= 5; i++){
+//     console.log(`Number: ${i}`)         //For
+// }
